e14fdb57ee5e7972cc53f189c104f5fc55d5f975
Iurie Guzun <iurie_guzun@yahoo.com>
2020-12-29 11:05:40 -0500

    Add subviews

--- GeomRead/ContentView.swift
+++ GeomRead/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            HStack(spacing: 10) {
+                Text("Left")
+                    .frame(width: geometry.size.width / 2, height: geometry.size.height)
+                    .background(Color.yellow)
+                Text("Right")
+                    .frame(width: geometry.size.width / 2, height: 50)
+                    .background(Color.orange)
+            }
+        }.padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
